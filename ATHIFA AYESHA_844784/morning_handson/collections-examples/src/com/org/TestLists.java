fd9ce5b93229b9d9c8cc3f79eed19bdceabaca19
Athifa Ayesha <athifaayesha2@gmail.com>
2020-11-29 00:57:49 -0800

    hands on

---
+++ ATHIFA AYESHA_844784/morning_handson/collections-examples/src/com/org/TestLists.java
@@ -0,0 +1,28 @@
+package com.org;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestLists {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(25);
+		list.add(35);
+		list.add(20);
+		list.add(30);
+		list.add(45);
+		for(int i:list){
+			System.out.println("item: "+i);
+		}
+		System.out.println("---filtering only even items---");
+		for(int i:list){
+			if(i %2 == 0){
+				System.out.println("item: "+i);
+			}
+		}
+
+	}
+
+}
---
+++ ATHIFA AYESHA_844784/morning_handson/collections-examples/src/com/org/TestMap.java
@@ -0,0 +1,17 @@
+package com.org;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestMap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map<String,String> map = new HashMap<String,String>();
+		map.put("username","root");
+		map.put("password","secretkey");
+		System.out.println(map);
+
+	}
+
+}
