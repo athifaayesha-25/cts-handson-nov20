19acbeec8166ba5d91faef52e6c1ffb7f6c7b826
Athifa Ayesha <athifaayesha2@gmail.com>
2020-12-04 15:29:16 +0530

    hands on and assignment

---
+++ ATHIFA AYESHA_844784/morning_handson/springboot/handson/spring-data-jpa/src/main/java/com/example/springdatajpa/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.example.springdatajpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// Note: You will not use @Repository, because repository is implemented by springboot
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+}
+
