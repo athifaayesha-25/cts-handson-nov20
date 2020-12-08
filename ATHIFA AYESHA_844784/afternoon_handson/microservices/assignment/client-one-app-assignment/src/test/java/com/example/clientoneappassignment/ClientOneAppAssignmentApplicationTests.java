c35b523bbd06c5e90a772f7cb8e9df4893d062aa
Athifa Ayesha <athifaayesha2@gmail.com>
2020-12-08 14:32:09 +0530

    assignment

---
+++ ATHIFA AYESHA_844784/afternoon_handson/microservices/assignment/client-one-app-assignment/src/test/java/com/example/clientoneappassignment/ClientOneAppAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientoneappassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientOneAppAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
