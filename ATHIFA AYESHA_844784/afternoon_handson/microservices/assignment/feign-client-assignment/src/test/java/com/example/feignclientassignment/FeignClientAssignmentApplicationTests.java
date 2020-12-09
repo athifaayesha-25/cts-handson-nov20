824372d34ea9751ac0d6b1417e3679b8b6b76665
Athifa Ayesha <athifaayesha2@gmail.com>
2020-12-09 16:24:10 +0530

    assignment

---
+++ ATHIFA AYESHA_844784/afternoon_handson/microservices/assignment/feign-client-assignment/src/test/java/com/example/feignclientassignment/FeignClientAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feignclientassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignClientAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
