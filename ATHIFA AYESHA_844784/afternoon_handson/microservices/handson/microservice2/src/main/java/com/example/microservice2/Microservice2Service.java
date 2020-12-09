eb5c06523d6f3afdf4ba0825f82ade32bf498168
Athifa Ayesha <athifaayesha2@gmail.com>
2020-12-09 12:54:18 +0530

    handson

---
+++ ATHIFA AYESHA_844784/afternoon_handson/microservices/handson/microservice2/src/main/java/com/example/microservice2/Microservice2Service.java
@@ -0,0 +1,12 @@
+package com.example.microservice2;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Microservice2Service {
+
+	public String greets() {
+		return "Hello Microservice2";
+	}
+}
+
